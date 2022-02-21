33ae19cca4c8cc5544832cf206522db60fa085ea
vidhi <vidhikakani1110@gmail.com>
2022-02-21 14:30:32 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+
+export const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+
+
+
---
+++ src/components/data.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const bookList=[
+  {
+    id:1,
+    title:"Reminders of Him: A Novel Paperback",
+    author: "Colleen Hoove",
+    img: "https://images-na.ssl-images-amazon.com/images/I/617uZq23IPL._AC_UL604_SR604,400_.jpg"
+  },
+  {
+    id:2,
+    title:"Red-Handed: How American Elites Get Rich Helping China Win Hardcover",
+    author: "Peter Schweize",
+    img:"https://images-na.ssl-images-amazon.com/images/I/71pD9ZR4N+L._AC_UL604_SR604,400_.jpg"
+  },
+  {
+    id:2,
+    title:"Red-Handed: How American Elites Get Rich Helping China Win Hardcover",
+    author: "Peter Schweize",
+    img:"https://images-na.ssl-images-amazon.com/images/I/71pD9ZR4N+L._AC_UL604_SR604,400_.jpg"
+  }
+]
+
+export const data = () => {
+  return (
+    <div></div>
+  )
+}
+
